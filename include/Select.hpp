f48cfbee39c9a495020d025272e26b7f3f27d60f
Liuyuan <2787141886@qq.com>
2023-10-11 21:41:46 +0800

    update Select.hpp

--- include/Select.hpp
+++ include/Select.hpp
@@ -8,31 +8,31 @@
 #include "global.hpp"
 #include "sort_algorithm_base.hpp"
 
+#include <cstddef>
 #include <vector>
 
 namespace my_algorithm {
 
 class select : public sort_algorithm_base {
 public:
-  void operator()(std::vector<int> &other) override;
+  void operator()(std::vector<int> &arr) override;
 };
 
 inline __attribute__((always_inline)) void
-select::operator()(std::vector<int> &other) {
-  auto next_swap_index = other.begin();
-  auto min = other.begin();
-
-  while (next_swap_index != other.end()) {
-    // find min
-    for (auto iter = next_swap_index; iter != other.end(); ++iter) {
-      if (*iter < *min) {
-        min = iter;
+select::operator()(std::vector<int> &arr) {
+  int index_min;
+  for (size_t index = 0; index < arr.size(); ++index) {
+    index_min = index;
+    for (size_t index_inner = index + 1; index_inner < arr.size();
+         ++index_inner) {
+      if (arr[index_inner] < arr[index_min]) {
+        index_min = index_inner;
       }
     }
 
-    std::swap(*min, *next_swap_index);
-    ++next_swap_index;
-    min = next_swap_index;
+    if (index_min != index) {
+      std::swap(arr[index], arr[index_min]);
+    }
   }
 }
 
